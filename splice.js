3f1388dd8928721fe6f2cb47d7f1e2389bfb5cc5
Shravan Kumar <kumarshravan12546@gmail.com>
2024-04-20 16:29:08 +0530

    splice function in javascript

---
+++ splice.js
@@ -0,0 +1,5 @@
+// Splice function
+let arr= [34,546,68,25,22,70,24,59,145];
+arr.splice(2,3,"hello","motu","patlu",46,26,79); //splice(start,how many elements should delete,to add element)
+// Note!!! Changes the value in orginal arrey
+console.log(arr);
