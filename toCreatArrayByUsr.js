1372108d1421e99d22526b374f048ad060da97c3
Shravan Kumar <kumarshravan12546@gmail.com>
2024-04-30 07:28:01 +0530

    To create array by user '

---
+++ toCreatArrayByUsr.js
@@ -0,0 +1,8 @@
+let n = prompt("Enter the number of element you want to enter in a array");
+    n = Number.parseInt(n);
+let arr=[]
+
+for(i=0;i<n;i++){
+     arr[i] = prompt("Enter the number");
+}   
+console.log(arr);
