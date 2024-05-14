0f89406744b9aafa8f45a08a91ef880d2828fe8d
Shravan Kumar U <kumarshravan12546@gmail.com>
2024-05-14 15:18:45 +0530

    promp operations in javascript

---
+++ toConvertTypeOfPrompt.js
@@ -0,0 +1,4 @@
+let a = prompt("Enter the number");
+a = Number.parseInt(a)
+alert(a+" "+typeof a);
+let n = prompt("Please enter your name ","Shravan") //Illi eradne addu default agi prompt alli ertahde browser open madvaga
